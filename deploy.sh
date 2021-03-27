982178066b7c2e40ac2ba3143e1ad42d7b2d9a64
paubric <paubric@gmail.com>
2021-03-27 20:58:46 +0200

    Clear everything

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-jekyll build
-git checkout gh-pages
-git --work-tree=_site add --all
-git --work-tree=_site commit -m "autogen: update site"
-git --work-tree=_site push -u origin gh-pages
-git checkout master
