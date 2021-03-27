67fdc350aeaf28ac73268715d9310fa99e76767e
paubric <paubric@gmail.com>
2021-03-27 20:34:59 +0200

    Update _site

---
+++ _site/deploy.sh
@@ -0,0 +1,10 @@
+git checkout -b gh-pages
+jekyll build
+git --work-tree=_site add --all
+git --work-tree=_site commit -m "autogen: update site"
+git --work-tree=_site pull origin gh-pages --rebase
+git --work-tree=_site push -u origin gh-pages
+git add .
+git stash
+git checkout master
+git branch -D gh-pages
